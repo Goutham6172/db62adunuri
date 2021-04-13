add9f15ebe517b2326410adf1671437db80e2be5
Goutham6172 <bhanukumar6172@gmail.com>
2021-04-13 00:57:08 -0500

    Added food detail page

--- controllers/food.js
+++ controllers/food.js
@@ -74,3 +74,18 @@ exports.food_view_all_Page = async function(req, res) {
     res.error(500,`{"error": ${err}}`);
     }
     };
+
+
+    // Handle a show one view with id specified by query
+exports.food_view_one_Page = async function(req, res) {
+    console.log("single view for id "  + req.query.id)
+    try{
+        result = await Food.findById( req.query.id)
+        res.render('fooddetail', 
+{ title: 'Food Detail', toShow: result });
+    }
+    catch(err){
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
--- routes/food.js
+++ routes/food.js
@@ -4,5 +4,5 @@ var router = express.Router();
 
 /* GET Foods. */
 router.get('/', foodcontroller.food_view_all_Page);
-
+router.get('/detail', foodcontroller.food_view_one_Page);
 module.exports = router;
