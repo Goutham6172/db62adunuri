f2595806885c78029673f6645fdc0b9e0d7525ff
Goutham6172 <bhanukumar6172@gmail.com>
2021-04-13 01:11:39 -0500

    added food create page

--- routes/food.js
+++ routes/food.js
@@ -5,4 +5,7 @@ var router = express.Router();
 /* GET Foods. */
 router.get('/', foodcontroller.food_view_all_Page);
 router.get('/detail', foodcontroller.food_view_one_Page);
+/* GET create costume page */
+router.get('/create', foodcontroller.food_create_Page);
+
 module.exports = router;
