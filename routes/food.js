8f9f3164659660e7fa42f93a4f7b443f0a773294
Goutham6172 <bhanukumar6172@gmail.com>
2021-04-13 01:20:07 -0500

    added update page

--- routes/food.js
+++ routes/food.js
@@ -7,5 +7,7 @@ router.get('/', foodcontroller.food_view_all_Page);
 router.get('/detail', foodcontroller.food_view_one_Page);
 /* GET create costume page */
 router.get('/create', foodcontroller.food_create_Page);
+/* GET create update page */
+router.get('/update', foodcontroller.food_update_Page);
 
 module.exports = router;
