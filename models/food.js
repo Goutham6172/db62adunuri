855b842980329145c9e0b6e988b138c1cc10f62a
Goutham6172 <bhanukumar6172@gmail.com>
2021-04-23 18:42:33 -0500

    authentication, validator, buttons

--- models/food.js
+++ models/food.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const foodSchema = mongoose.Schema({
     foodquantity: String,
-    foodprice: Number,
+    foodprice: {type:Number, min:[4,'Minimum quantity is 4'],max:[25,'Maximum quantity is 4']},
     foodcolor: String
 })
 
--- routes/food.js
+++ routes/food.js
@@ -7,7 +7,18 @@ router.get('/', foodcontroller.food_view_all_Page);
 router.get('/detail', foodcontroller.food_view_one_Page);
 /* GET create costume page */
 router.get('/create', foodcontroller.food_create_Page);
-/* GET create update page */
-router.get('/update', foodcontroller.food_update_Page);
+
+// A little function to check if we have an authorized user and continue on
+// or
+// redirect to login.
+const secured = (req, res, next) => {
+    if (req.user){
+     return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+    }
+/* GET update food page */
+router.get('/update', secured, foodcontroller.food_update_Page);
 router.get('/delete', foodcontroller.food_delete_Page);
 module.exports = router;
