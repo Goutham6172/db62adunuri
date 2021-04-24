9075afbde2041b2d759b8fd81e16bccd1e54d8e0
Goutham6172 <bhanukumar6172@gmail.com>
2021-04-23 19:00:54 -0500

    added bonus

--- models/food.js
+++ models/food.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 const foodSchema = mongoose.Schema({
-    foodquantity: String,
+    foodquantity: {type:Stringg,required:[true,'Quantity required'],enum:['Large','Small','Medium']},
     foodprice: {type:Number, min:[4,'Minimum quantity is 4'],max:[25,'Maximum quantity is 4']},
-    foodcolor: String
+    foodcolor: {type:String,required:[true,'color required']}
 })
 
 module.exports = mongoose.model("food",foodSchema)
--- routes/food.js
+++ routes/food.js
@@ -5,8 +5,6 @@ var router = express.Router();
 /* GET Foods. */
 router.get('/', foodcontroller.food_view_all_Page);
 router.get('/detail', foodcontroller.food_view_one_Page);
-/* GET create costume page */
-router.get('/create', foodcontroller.food_create_Page);
 
 // A little function to check if we have an authorized user and continue on
 // or
@@ -20,5 +18,9 @@ const secured = (req, res, next) => {
     }
 /* GET update food page */
 router.get('/update', secured, foodcontroller.food_update_Page);
-router.get('/delete', foodcontroller.food_delete_Page);
+
+/* GET create food page */
+router.get('/create',secured, foodcontroller.food_create_Page);
+
+router.get('/delete', secured, foodcontroller.food_delete_Page);
 module.exports = router;
