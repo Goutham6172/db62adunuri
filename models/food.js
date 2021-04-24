f06fdb11b2be4a83cbb9b2d247703b9796691a47
Goutham6172 <bhanukumar6172@gmail.com>
2021-04-23 19:31:26 -0500

    made correction

--- models/food.js
+++ models/food.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const foodSchema = mongoose.Schema({
-    foodquantity: {type:Stringg,required:[true,'Quantity required'],enum:['Large','Small','Medium']},
+    foodquantity: {type:String,required:[true,'Quantity required'],enum:["Large","Small","Medium"]},
     foodprice: {type:Number, min:[4,'Minimum quantity is 4'],max:[25,'Maximum quantity is 4']},
     foodcolor: {type:String,required:[true,'color required']}
 })
